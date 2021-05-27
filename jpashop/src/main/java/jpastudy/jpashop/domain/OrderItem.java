003b2dd10f8c3987d80a70ae7a3b675d100a2290
choiinho <ino0319@gmail.com>
2021-05-28 00:09:22 +0900

    cascade

--- jpashop/src/main/java/jpastudy/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpastudy/jpashop/domain/OrderItem.java
@@ -3,6 +3,8 @@ package jpastudy.jpashop.domain;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class OrderItem extends BaseEntity {
 
@@ -11,11 +13,11 @@ public class OrderItem extends BaseEntity {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 
