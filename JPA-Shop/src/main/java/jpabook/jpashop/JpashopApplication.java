0685890573f54b2b5063bd16eaa7a80caf91bb39
Inho.Choi <choiinho@knou.ac.kr>
2021-07-10 11:03:20 +0900

    setting

---
+++ JPA-Shop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, ino, Inc. All rights reserved.
+ */
+
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
---
+++ JPA-Shop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021, ino, Inc. All rights reserved.
+ */
+
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
