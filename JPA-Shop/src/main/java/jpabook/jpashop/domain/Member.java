15a0dcc19fbdf7946373a516e7b99dba66dc41ba
Inho.Choi <choiinho@knou.ac.kr>
2021-08-27 13:00:29 +0900

    주문 수정

--- JPA-Shop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA-Shop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +30,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
