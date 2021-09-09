63d0d7e7d7b17456583bdef321ed845068e95606
Inho.Choi <choiinho@knou.ac.kr>
2021-09-09 11:19:41 +0900

    엔티티를 DTO로 변환 페이징과 한계 돌파

--- JPA-Shop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA-Shop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -40,6 +41,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+//    @BatchSize(size = 1000)
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
