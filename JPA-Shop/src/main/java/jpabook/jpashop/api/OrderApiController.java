6330de4d11cdbfb6452323473e874aa858a2a88e
Inho.Choi <choiinho@knou.ac.kr>
2021-09-14 09:35:27 +0900

    JPA에서 DTO 직접 조회

--- JPA-Shop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ JPA-Shop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -69,11 +72,18 @@ public class OrderApiController {
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
+
         return result;
     }
 
-    @Data
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @Getter
     static class OrderDto {
+
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
